6a92b8449c980d628a5110f13865211da8ec30f4
lokey0905 <lokey0905@gmail.com>
2022-12-20 09:39:13 +0800

    update 10-3

--- ch10/hw10-3/hw10-3.c
+++ ch10/hw10-3/hw10-3.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <io.h>
+#include <fcntl.h>
+#include <string.h>
+
 int main(void)
 {
 	FILE* pfile; // 宣告指向檔案的結構指標
@@ -9,25 +15,28 @@ int main(void)
 	int iNum = 0;  // 計算英文字母的個數
 	int a = 0, e = 0, i = 0, o = 0, u = 0;
 	int sum[26] = { 0 };
+	int ifd, ilen;/*
 	if ((pfile = fopen("..//prog10-3//story.txt", "r")) == NULL) {  // 判斷檔案是否開啟失敗
 		printf("..//prog10-3//story.txt 檔案無法開啟"); system("pause"); return(0);
+	}*/
+	ifd = _open("..//prog10-3//story.txt", O_RDWR);
+	if (ifd == -1) {
+		printf("..//prog10-3//story.txt 檔案無法開啟"); system("pause"); return(0);
 	}
-	while (!feof(pfile)) {	// 判斷是否讀到檔案尾
-		ch = fgetc(pfile);	// 讀取一個字元
-		//putchar(ch);		// 輸出讀取的字元
-		//if (isalpha(ch)) iNum++; //如果是英文字母 iNum 就加1
+	
+	_lseek(ifd, 0, SEEK_SET);		 // 讀寫頭回到檔案頭
 
-
+	while (!_eof(ifd)) {	// 判斷是否讀到檔案尾
+		_read(ifd, &ch, sizeof(char));
 		if (ch >= 'a') {
 			sum[ch - 'a']++;
 		}
 		else sum[ch - 'A']++;
 	}
-	//printf("\n\nstory.txt 共包含%3d 個英文字母\n", iNum);
-	//printf("\n\n");
+	
 	for (int ii = 0;ii < 26;ii++) {
 		printf("字母 %c或%c 共出現%4d次\n", ii + 'A', ii + 'a', sum[ii]);
 	}
-	fclose(pfile); // 關閉檔案
+	_close(ifd); // 關閉檔案
 	system("pause"); return(0);
 }
--- ch10/prog10-18/prog10-18.c
+++ ch10/prog10-18/prog10-18.c
@@ -10,13 +10,13 @@ int main(void)
 	if ((pfile = fopen("number.bin", "rb")) == NULL) {
 		printf("number.bin 檔案無法開啟"); system("pause"); return(0);
 	} //開啟二進位檔
-	/*
+	
 	fread(&da, sizeof(double), 1, pfile);  // 讀取變數 da 的內容
 	fread(&fb, sizeof(float), 1, pfile); // 讀取變數 fb 的內容  
 	fread(iArray, sizeof(int), 3, pfile); // 讀取 iArray 的內容
 	printf("da = %6.4f\n", da);
 	printf("fb = %6.4f\n", fb);
 	for (i = 0; i < 3; i++) printf("iArray[%d]=%d\n", i, iArray[i]);
-	*/
+	
 	fclose(pfile); system("pause"); return(0); // 關閉檔案
 }
